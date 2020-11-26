0829c355749d34709f049ef4e6f610df9ffdf821
Zain <reply2zain@gmail.com>
2020-11-26 16:26:45 -0500

    updated depoloyment to heroku

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/cs4550-f20-client-angular-aaban'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/myapp/index.html'));});
+    '/dist/cs4550-f20-client-angular-aaban/index.html'));});
 app.listen(process.env.PORT || 8080);
