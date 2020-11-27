23f7338fcc5737ad0f5af3cc21afc1ca8c3d1a69
Zain <reply2zain@gmail.com>
2020-11-27 01:54:34 -0500

    updates for heroku

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/cs4550-f20-client-angular-aaban'));
+app.use(express.static(__dirname + '/dist'));
+app.listen(process.env.PORT || 8080);
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/cs4550-f20-client-angular-aaban/index.html'));});
-app.listen(process.env.PORT || 8081);
+    '/dist/index.html'));});
+
+console.log('Console listening')
